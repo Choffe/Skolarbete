839b9de02de1e3098510f803655cd4249739e375
Kristofer Jacobson <kristofer.jacobson@gmail.com>
2012-02-01 15:02:34 +0100

    first setup

---
+++ src/dbtLab3/Database.java
@@ -0,0 +1,206 @@
+package dbtLab3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ * Database is a class that specifies the interface to the movie database. Uses
+ * JDBC and the MySQL Connector/J driver.
+ */
+public class Database {
+	/**
+	 * The database connection.
+	 */
+	private Connection conn;
+
+	/**
+	 * An SQL statement object.
+	 */
+	private Statement stmt;
+
+	/**
+	 * Create the database interface object. Connection to the database is
+	 * performed later.
+	 */
+	public Database() {
+		conn = null;
+	}
+
+	/**
+	 * Open a connection to the database, using the specified user name and
+	 * password.
+	 * 
+	 * @param userName
+	 *            The user name.
+	 * @param password
+	 *            The user's password.
+	 * @return true if the connection succeeded, false if the supplied user name
+	 *         and password were not recognized. Returns false also if the JDBC
+	 *         driver isn't found.
+	 */
+	public boolean openConnection(String userName, String password) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://puccini.cs.lth.se/" + userName, userName,
+					password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Close the connection to the database.
+	 */
+	public void closeConnection() {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+		}
+		conn = null;
+	}
+
+	/**
+	 * Check if the connection to the database has been established
+	 * 
+	 * @return true if the connection has been established
+	 */
+	public boolean isConnected() {
+		return conn != null;
+	}
+
+	/* --- insert own code here --- */
+
+	public ArrayList<String> getMovies(){
+		String sql = "select name from movie;";
+		PreparedStatement ps = null;
+		ArrayList<String> result = new ArrayList<String>();
+		try {
+			ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				result.add(rs.getString("name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}finally{
+			try {
+				ps.close();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		return result;
+		
+	}
+	public ArrayList<String> getShowdates(String movieName){
+		String sql = "select showdate from performance where moviename = ?";
+		PreparedStatement ps = null;
+		ArrayList<String> result = new ArrayList<String>();
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, movieName);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				result.add(rs.getString("showdate"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}finally{
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+		
+	}
+	
+	//Haha refaktorisera lite eller???
+	public Performance getPerformance(String movieName, String showdate){
+		String sql = "select * from performance where moviename = ? and showdate= ?";
+		PreparedStatement ps = null;
+		Performance performance = new Performance();
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, movieName);
+			ps.setString(2, showdate);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			performance.setShowdate(rs.getString("showdate"));
+			performance.setShowtime(rs.getString("showtime"));
+			performance.setBookedSeats(rs.getInt("bookedSeats"));
+			performance.setMovieName(rs.getString("moviename"));
+			performance.setTheaterName(rs.getString("theatername"));
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}finally{
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return performance;
+	}
+	
+	public boolean makeReservation(Performance p){
+	
+		String sql = "select bookedSeats from performance where moviename = ? and showdate= ?";
+		PreparedStatement ps = null;
+		try {
+			conn.setAutoCommit(false);
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, p.getMovieName());
+			ps.setString(2, p.getShowdate());
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			if(rs.getInt("bookedSeats") >0){
+				ps = conn.prepareStatement("update performance set bookedSeats = bookedSeats -1" +
+											"where moviename = " + p.getMovieName()+ " and "+
+											" showdate = "+ p.getShowdate());
+				if(ps.executeUpdate() != 1){
+					conn.rollback();
+					return false;
+				}
+				
+//				ps = conn.prepareStatement("insert into reservationmakesfor );
+			}else{
+				conn.rollback();
+				return false;
+			}
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}finally{
+			try {
+				ps.close();
+				conn.setAutoCommit(true);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return true;
+	}
+	
+}
---
+++ src/dbtLab3/Performance.java
@@ -0,0 +1,44 @@
+package dbtLab3;
+
+public class Performance {
+
+	private String movieName;
+	private String showtime;
+	private String showdate;
+	private int bookedSeats;
+	private String theaterName;
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	public String getShowtime() {
+		return showtime;
+	}
+	public void setShowtime(String showtime) {
+		this.showtime = showtime;
+	}
+	public String getShowdate() {
+		return showdate;
+	}
+	public void setShowdate(String showdate) {
+		this.showdate = showdate;
+	}
+	public int getBookedSeats() {
+		return bookedSeats;
+	}
+	public void setBookedSeats(int bookedSeats) {
+		this.bookedSeats = bookedSeats;
+	}
+	public String getTheaterName() {
+		return theaterName;
+	}
+	public void setTheaterName(String theaterName) {
+		this.theaterName = theaterName;
+	}
+	
+
+
+
+}
